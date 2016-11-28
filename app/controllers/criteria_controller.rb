a8beb6da2d225533226f46a46feea2bd94cb7a70
4rlm <4rlm@protonmail.ch>
2016-11-27 19:59:05 -0600

    Core Accounts and Domain Finder tables changed Bootstrap style. And changed order of criteria columns.

--- app/controllers/criteria_controller.rb
+++ app/controllers/criteria_controller.rb
@@ -1,12 +1,12 @@
 class CriteriaController < ApplicationController
     def all_criteria
-        @all_exclude_root = ExcludeRoot.all
-        @all_in_host_del =   InHostDel.all
-        @all_in_host_neg =  InHostNeg.all
-        @all_in_host_po =   InHostPo.all
-        @all_in_suffix_del = InSuffixDel.all
-        @all_in_text_del =   InTextDel.all
-        @all_in_text_neg =  InTextNeg.all
-        @all_in_text_po =    InTextPo.all
+        @all_exclude_root = ExcludeRoot.order(term: :asc)
+        @all_in_host_del =   InHostDel.order(term: :asc)
+        @all_in_host_neg =  InHostNeg.order(term: :asc)
+        @all_in_host_po =   InHostPo.order(term: :asc)
+        @all_in_suffix_del = InSuffixDel.order(term: :asc)
+        @all_in_text_del =   InTextDel.order(term: :asc)
+        @all_in_text_neg =  InTextNeg.order(term: :asc)
+        @all_in_text_po =    InTextPo.order(term: :asc)
     end
 end
