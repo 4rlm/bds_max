bc5c56b19d980dddb92180080bc6025c0d0a331f
4rlm <4rlm@protonmail.ch>
2016-12-04 12:18:48 -0600

    Added number formattings | Dashboard Statistical Summary panel

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
     def index
+        # Core Instance Variables
+        @core_all = Core.count
+        @domainer_query_count = get_domainer_query_count
+        @core_matched_count = Core.where(bds_status: "Matched").count
+        @core_dom_result_count = Core.where(bds_status: "Dom Result").count
+
+        # Gcse Instance Variables
+        @gcse_all = Gcse.count
     end
 
     def search_result_core
@@ -11,5 +19,17 @@ class SearchController < ApplicationController
     def search_result_gcse
         set_selected_status_gcse({domain_status: params[:domain_status]})
         redirect_to gcses_path
+    end
+
+    private
+
+    def get_domainer_query_count
+        counter = 0
+        Core.all.each do |core|
+            if core.domainer_date
+                counter += 1
+            end
+        end
+        counter
     end
 end
