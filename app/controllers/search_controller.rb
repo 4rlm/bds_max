70fc86669ca3d6afeaf39f8255b03e269aaf3972
4rlm <4rlm@protonmail.ch>
2016-12-04 18:29:47 -0600

    Changed Dashboard view and added criteria

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,27 @@ class SearchController < ApplicationController
         @core_matched_count = Core.where(bds_status: "Matched").count
         @core_dom_result_count = Core.where(bds_status: "Dom Result").count
 
+        @urls_updated = Core.where(url_comparison: "Different").count
+        @roots_updated = Core.where(root_comparison: "Different").count
+
         # Gcse Instance Variables
         @gcse_all = Gcse.count
+
+        # Solitaries Instance Variables
+         @solitary_all = Solitary.count
     end
 
     def search_result_core
         # {bds_status: params[:bds_status], sfdc_type: params[:sfdc_type], col_name: params[:col_name]}
-        set_selected_status_core({bds_status: params[:bds_status], sfdc_type: params[:sfdc_type]})
+        set_selected_status_core({bds_status: params[:bds_status], url_comparison: params[:url_comparison], root_comparison: params[:root_comparison], sfdc_type: params[:sfdc_type], sfdc_sales_person: params[:sfdc_sales_person], sfdc_tier: params[:sfdc_tier], sfdc_ult_grp: params[:sfdc_ult_grp], sfdc_group: params[:sfdc_group], sfdc_city: params[:sfdc_city], sfdc_state: params[:sfdc_state], sfdc_zip: params[:sfdc_zip]})
         redirect_to cores_path
     end
 
     def search_result_gcse
-        set_selected_status_gcse({domain_status: params[:domain_status]})
+        set_selected_status_gcse({domain_status: params[:domain_status], gcse_result_num: params[:gcse_result_num], gcse_timestamp: params[:gcse_timestamp], sfdc_ult_acct: params[:sfdc_ult_acct], sfdc_acct: params[:sfdc_acct], sfdc_type: params[:sfdc_type], sfdc_city: params[:sfdc_city], sfdc_state: params[:sfdc_state]})
         redirect_to gcses_path
     end
+
 
     private
 
