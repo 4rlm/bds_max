8e0ae2effcee76be72aaf7b084dcde29615c5b83
Adam Booth <4rlm@protonmail.ch>
2017-02-04 15:23:11 -0600

    Create user authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+    after_initialize :init
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+    enum role: [:basic, :intermediate, :advanced, :admin]
+
+    def init
+        self.role ||= :basic if self.has_attribute? :role
+    end
 end
