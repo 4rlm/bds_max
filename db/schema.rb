39a196533410fdccaeae4ed983a9e5b229d1f344
Adam Booth <4rlm@protonmail.ch>
2017-02-04 23:49:35 -0600

    fix heroku db and sync db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204172036) do
+ActiveRecord::Schema.define(version: 20170205054734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -351,6 +351,7 @@ ActiveRecord::Schema.define(version: 20170204172036) do
     t.string   "work_phone"
     t.string   "mobile_phone"
     t.integer  "role"
+    t.string   "department"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
