88e76d442acd0a4a34e57e2844903a005e8e6252
Adam Booth <4rlm@protonmail.ch>
2017-02-04 22:50:06 -0600

    migration file fix for heroku

--- db/migrate/20170117020736_remove_columns_from_locations4.rb
+++ db/migrate/20170117020736_remove_columns_from_locations4.rb
@@ -1,5 +1,5 @@
 class RemoveColumnsFromLocations4 < ActiveRecord::Migration[5.0]
   def change
-    remove_column :locations, :address, :string
+    # remove_column :locations, :address, :string
   end
 end
