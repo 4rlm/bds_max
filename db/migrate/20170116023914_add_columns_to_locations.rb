137ed037278a652a82e4e5b452cdec305e9bc34d
Adam Booth <4rlm@protonmail.ch>
2017-02-04 22:45:29 -0600

    migration file fix for heroku

--- db/migrate/20170116023914_add_columns_to_locations.rb
+++ db/migrate/20170116023914_add_columns_to_locations.rb
@@ -1,6 +1,6 @@
 class AddColumnsToLocations < ActiveRecord::Migration[5.0]
   def change
-      add_column :locations, :address, :string
+    #   add_column :locations, :address, :string
       add_column :locations, :city, :string
       add_column :locations, :state, :string
       add_column :locations, :state_code, :string
