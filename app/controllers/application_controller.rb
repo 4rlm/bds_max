941e19247498f0e471843d065633d68f0e9dfa8d
Adam Booth <4rlm@protonmail.ch>
2017-02-05 17:16:27 -0600

    Devise View update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,47 @@
 require_relative '../services/core_service'
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
 
-  def set_selected_status_core(choice_hash)
-      $core_choice_hash = choice_hash
-  end
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def get_selected_status_core
-      $core_choice_hash
-  end
+    def set_selected_status_core(choice_hash)
+        $core_choice_hash = choice_hash
+    end
 
-  def set_selected_status_gcse(choice_hash)
-    $gcse_choice_hash = choice_hash
-  end
+    def get_selected_status_core
+        $core_choice_hash
+    end
 
-  def get_selected_status_gcse
-    $gcse_choice_hash
-  end
+    def set_selected_status_gcse(choice_hash)
+        $gcse_choice_hash = choice_hash
+    end
 
-  def set_selected_status_staffer(choice_hash)
-    $staffer_choice_hash = choice_hash
-  end
+    def get_selected_status_gcse
+        $gcse_choice_hash
+    end
 
-  def get_selected_status_staffer
-    $staffer_choice_hash
-  end
+    def set_selected_status_staffer(choice_hash)
+        $staffer_choice_hash = choice_hash
+    end
+
+    def get_selected_status_staffer
+        $staffer_choice_hash
+    end
 
     def set_selected_status_location(choice_hash)
-      $location_choice_hash = choice_hash
+        $location_choice_hash = choice_hash
     end
 
     def get_selected_status_location
-      $location_choice_hash
+        $location_choice_hash
     end
 
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :work_phone, :mobile_phone, :department])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :work_phone, :mobile_phone, :department])
+    end
 end
