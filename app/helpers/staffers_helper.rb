aa8873f59ba31e18745b31d2d0f3d4def8f8ecb7
Gahee Heo <ghbooth12@gmail.com>
2017-03-13 15:40:25 -0500

    [gh] Staffer detail view: group with sfdc_id vs sfdc_clean_url

--- app/helpers/staffers_helper.rb
+++ app/helpers/staffers_helper.rb
@@ -1,30 +1,20 @@
 module StaffersHelper
-    # def staffer_status_list
-    #     ["Ready", "Search Error", "Temp Error", "Queued", "No Matches", 'Try Again', "Scraped", 'Verified', 'Destroy', "Matched"]
-    # end
-    #
-    # def franchise_list
-    #     ['Acura', 'Alfa', 'Aston', 'Audi', 'Auto', 'Autogroup', 'Automall', 'Automotive', 'Autoplex', 'Autos', 'Autosales', 'Bentley', 'Benz', 'Bmw', 'Bugatti', 'Buick', 'Cadillac', 'Cars', 'Cdjr', 'Chev', 'Chevrolet', 'Chevy', 'Chrysler', 'Cjd', 'Corvette', 'Daewoo', 'Dealer', 'Dodge', 'Ferrari', 'Fiat', 'Ford', 'Gm', 'Gmc', 'Group', 'Highline', 'Honda', 'Hummer', 'Hyundai', 'Imports', 'Infiniti', 'Isuzu', 'Jaguar', 'Jeep', 'Kia', 'Lamborghini', 'Lexus', 'Lincoln', 'Lotus', 'Maserati', 'Mazda', 'Mb', 'Mclaren', 'Mercedes', 'Mercury', 'Mini', 'Mitsubishi', 'Motor', 'Motors', 'Nissan', 'Oldsmobile', 'Plymouth', 'Pontiac', 'Porsche', 'Ram', 'Range', 'Rolls', 'Rover', 'Royce', 'Saab', 'Saturn', 'Scion', 'Smart', 'Subaru', 'Superstore', 'Suzuki', 'Toyota', 'Trucks', 'Usedcars', 'Volkswagen', 'Volvo', 'Vw']
-    # end
-    #
-    # def template_list
-    #     ['Dealer.com', 'DealerOn', 'Cobalt', 'DealerFire', 'DealerInspire', ["None", nil]]
-    # end
+    def grap_right_core(staffer)
+        src = staffer.cont_source
+        if src == "CRM"
+            Core.find_by(sfdc_id: staffer.sfdc_id)
+        elsif src == "Web"
+            Core.find_by(sfdc_clean_url: staffer.domain)
+        end
+    end
 
-    # def influence_list
-    #     ["Decision Influencer", "Decision Maker", "Other", "N/A", ["None", nil]]
-    # end
-    #
-    # def inactive_list
-    #     [["Active", "0"], ["Inactive", "1"]]
-    # end
-    #
-    # def contact_status_list
-    #     ["Scraped", "SFDC", "Merged", "Merge-Request", "Hide", "Destroy"]
-    # end
-    #
-    # def contact_source_list
-    #     ["Dealer Site", "SFDC", "Other"]
-    # end
+    def grap_right_staffers(staffer)
+        src = staffer.cont_source
+        if src == "CRM"
+            Staffer.where(sfdc_id: staffer.sfdc_id)
+        elsif src == "Web"
+            Staffer.where(domain: staffer.domain)
+        end
+    end
 
 end
