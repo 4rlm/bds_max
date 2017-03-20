7a2ec993304f2de166aaedece04cdff91f762795
Adam Booth <4rlm@protonmail.ch>
2017-03-20 15:15:50 -0500

    finished core account profile.

--- app/helpers/cores_helper.rb
+++ app/helpers/cores_helper.rb
@@ -9,15 +9,15 @@ module CoresHelper
             HTML
         elsif sts == "Flag"
             html = <<-HTML
-                <td class='bg-orange geo w-xsm''>#{fa_icon 'flag', class: 'fa-white'}</td>
+                <td class='bg-orange geo w-xsm'>#{fa_icon 'flag', class: 'fa-white'}</td>
             HTML
         elsif sts == "Drop"
             html = <<-HTML
-                <td class='bg-red geo w-xsm''>#{fa_icon 'minus', class: 'fa-white'}</td>
+                <td class='bg-red geo w-xsm'>#{fa_icon 'minus', class: 'fa-white'}</td>
             HTML
         else
             html = <<-HTML
-                <td geo w-xsm'><span></span></td>
+
             HTML
         end
         html.html_safe
