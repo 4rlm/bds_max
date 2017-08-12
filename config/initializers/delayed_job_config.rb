1462576f2da3fa16e542d55a2d5bbd686f0bbdb5
Adam Booth <4rlm@protonmail.ch>
2017-08-11 22:35:11 -0500

    delayed job configuration and testing.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -11,11 +11,11 @@
 
 ###############
 
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 10
+# Delayed::Worker.destroy_failed_jobs = false
+# Delayed::Worker.sleep_delay = 10
 Delayed::Worker.max_attempts = 1
 # Delayed::Worker.max_run_time = 2.minutes
-Delayed::Worker.read_ahead = 10
+# Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 # Delayed::Worker.delay_jobs = !Rails.env.test?
 # Delayed::Worker.raise_signal_exceptions = :term
