ce71b68d631e764cf44abe3307e621e57c2ba1bc
Adam Booth <4rlm@protonmail.ch>
2017-08-21 16:01:38 -0500

    Created ComplexQueryIterator Module to handle all iterations from ContactScraper.  Will also use it in all other classes for default iterator.
    * started creating PageFinder class and module.
    
    * almost finished with TemplateFinder class, but need to refactor InternetConnectionValidator to include error message case statement, currently in StafferService.
    
    * same as previous.  forgot to include this file to commit.
    
    * figured out important logic for staffer iterator in batches to pause.
    
    * almost completed with staffers regulating pace of large query into delayed jobs.
    
    * PERFECT DJ BATCH solution in IndexerServices to use in all other iterations.
    
    * Good Save Point.  Calibrated DJ for Staffers and adjusted pool threads from 25 to 15
    
    * created ContactScraper in lib.  moved out of StafferService.
    
    * segmented new ContactScraper class and prepared to move iterator methods into a new module.
    
    * Created ComplexQueryIterator Module to handle all iterations from ContactScraper.  Will also use it in all other classes for default iterator.

---
+++ lib/servicers/contact_scraper.rb
@@ -0,0 +1,100 @@
+require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
+require 'delayed_job'
+require 'staffer_helper/cs_helper'
+require 'staffer_helper/cobalt_cs'
+require 'staffer_helper/dealer_com_cs'
+require 'staffer_helper/dealer_direct_cs'
+require 'staffer_helper/dealer_eprocess_cs'
+require 'staffer_helper/dealer_inspire_cs'
+require 'staffer_helper/dealerfire_cs'
+require 'staffer_helper/dealeron_cs'
+require 'staffer_helper/standard_scraper'
+require 'indexer_helper/rts/rts_manager'
+
+# Bridges ContactScraper Class to StafferService Class.
+require 'delayed_job'
+
+# Call: StafferService.new.start_contact_scraper
+# Call: ContactScraper.new.cs_starter
+
+class ContactScraper
+  include InternetConnectionValidator
+  include ComplexQueryIterator
+
+  def initialize
+    puts "\n\n== Welcome to the ContactScraper Class! ==\n\n"
+    @query_limit = 10 #=> Number of rows per batch in raw_query.
+
+    ## Below are Settings for ComplexQueryIterator Module.
+    @dj_wait_time = 3 #=> How often to check dj queue count.
+    @dj_count_limit = 0 #=> Num allowed before releasing next batch.
+    @number_of_groups = 2 #=> Divide query into groups of x.
+  end
+
+  def cs_starter
+    generate_query
+  end
+
+  def generate_query
+    # .where.not(staff_url: nil, contact_status: "CS Result")
+
+    raw_query = Indexer
+    .select(:id)
+    .where("template NOT LIKE '%Error%'")
+    .where(contact_status: "CS Result")
+    .where('scrape_date <= ?', Date.today - 1.day)
+
+    iterate_raw_query(raw_query) #=> Method is in ComplexQueryIterator.
+  end
+
+  #############################################
+  ## ComplexQueryIterator takes raw_query and creates series of forked iterations based on limits established above in initialize method.  Then it calls 'template_starter(id)' method.  Module serves as bridge for iteration work.
+  #############################################
+
+  def view_indexer_current_db_info(indexer)
+    puts "\n=== Current DB Info ===\n"
+    puts "indexer_status: #{indexer.indexer_status}"
+    puts "template: #{indexer.template}"
+    puts "staff_url: #{indexer.staff_url}"
+    puts "web_staff_count: #{indexer.web_staff_count}"
+    puts "scrape_date: #{indexer.scrape_date}"
+    puts "#{"="*30}\n\n"
+  end
+
+  def template_starter(id)
+    indexer = Indexer.find(id)
+    view_indexer_current_db_info(indexer)
+    url = indexer.staff_url
+    start_mechanize(url) #=> returns @html
+    html = @html
+
+    begin
+      template = indexer.template
+      case template
+      when "Dealer.com"
+        DealerComCs.new.contact_scraper(html, url, indexer)
+      when "Cobalt"
+        CobaltCs.new.contact_scraper(html, url, indexer)
+      when "DealerOn"
+        DealeronCs.new.contact_scraper(html, url, indexer)
+      when "Dealer Direct"
+        DealerDirectCs.new.contact_scraper(html, url, indexer)
+      when "Dealer Inspire"
+        DealerInspireCs.new.contact_scraper(html, url, indexer)
+      when "DealerFire"
+        DealerfireCs.new.contact_scraper(html, url, indexer)
+      when "DEALER eProcess"
+        DealerEprocessCs.new.contact_scraper(html, url, indexer)
+      else
+        StandardScraperCs.new.contact_scraper(html, url, indexer)
+      end
+    rescue
+      puts "\n\n== CS Error!! ==\n\n"
+      puts @error_code
+      # indexer.update_attributes(indexer_status: "ContactScraper", contact_status: @error_code, scrape_date: DateTime.now)
+    end ## rescue ends
+  end
+
+end
