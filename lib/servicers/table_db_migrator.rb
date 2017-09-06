d1e70759b4184bf5db9217fd99991b17bb738e62
Adam Booth <4rlm@protonmail.ch>
2017-09-06 17:32:08 -0500

    completed TableDbMigrator.  working very well.  next step is to create polymorphic with Url, Phone, Address, Organization.

--- lib/servicers/table_db_migrator.rb
+++ lib/servicers/table_db_migrator.rb
@@ -4,20 +4,40 @@
 class TableDbMigrator
 
   def initialize
-    # puts "\n\n#{"="*40}\n== Starting TableDbMigrator ==\nMigrates Organization, Address, Url, Phone from Core, Indexer, Location, Who\nAll relevant data will be stored in these common db tables, rather than in the old separate system.\nTHERE SHOULD BE NO DUPLICATES ANYWHERE IN ENTIRE DB!!\n#{"="*40}\n"
-
-    # @url_fields_array = [:url]
-    # @phone_fields_array = [:phone]
-    # @organization_fields_array = [:name]
-    @address_fields_array = [:full_addr, :street, :city, :state, :zip, :addr_pin]
+    puts "\n\n#{"="*40}\n== Starting TableDbMigrator ==\nMigrates Organization, Address, Url, Phone from Core, Indexer, Location, Who\nAll relevant data will be stored in these common db tables, rather than in the old separate system.\nTHERE SHOULD BE NO DUPLICATES ANYWHERE IN ENTIRE DB!!\n#{"="*40}\n"
   end
 
   def td_starter
-    # generate_queries
-    # migrate_urls_starter #=> PERFECT!
-    # migrate_phones_starter #=> PERFECT!
-    migrate_organizations_starter
+    migrate_urls_starter #=> PERFECT!
+    migrate_phones_starter #=> PERFECT!
+    migrate_organizations_starter #=> PERFECT!
+    migrate_addresses_starter #=> PERFECT!
   end
+
+  ############################
+  def migrate_addresses_starter
+    address_attrs = [:street, :city, :state, :zip, :full_addr, :addr_pin]
+    core_attrs = [:sfdc_street, :sfdc_city, :sfdc_state, :sfdc_zip, :full_address, :crm_acct_pin]
+    indexer_attrs = [:street, :city, :state, :zip, :full_addr, :acct_pin]
+    location_attrs = [:street, :city, :state_code, :postal_code, :geo_full_addr, :geo_acct_pin]
+    who_attrs = [:registrant_address, :registrant_city, :registrant_state, :registrant_zip, :who_addr_pin]
+
+    all_addresses = Address.select(address_attrs).all.map { |row| {street: row.street, city: row.city, state: row.state, zip: row.zip, full_addr: row.full_addr, addr_pin: row.addr_pin} }.uniq
+    original_addresses = all_addresses
+
+    all_addresses += Core.select(core_attrs).all.map { |row| {street: row.sfdc_street, city: row.sfdc_city, state: row.sfdc_state, zip: row.sfdc_zip, full_addr: row.full_address, addr_pin: row.crm_acct_pin} }.uniq
+
+    all_addresses += Indexer.select(indexer_attrs).all.map { |row| {street: row.street, city: row.city, state: row.state, zip: row.zip, full_addr: row.full_addr, addr_pin: row.acct_pin} }.uniq
+
+    all_addresses += Location.select(location_attrs).all.map { |row| {street: row.street, city: row.city, state: row.state_code, zip: row.postal_code, full_addr: row.geo_full_addr, addr_pin: row.geo_acct_pin} }.uniq
+
+    all_addresses += Who.select(who_attrs).all.map { |row| {street: row.registrant_address, city: row.registrant_city, state: row.registrant_state, zip: row.registrant_zip, full_addr: nil, addr_pin: row.who_addr_pin} }.uniq
+
+    hashes_to_save = all_addresses - original_addresses
+
+    Address.transaction { hashes_to_save.each { |data_row_hash| Address.new(data_row_hash).save } } #=> Very fast!
+  end
+
 
   ############################
   def migrate_urls_starter #=> PERFECT!
@@ -64,7 +84,7 @@ class TableDbMigrator
   end
 
   ############################
-  def migrate_organizations_starter
+  def migrate_organizations_starter #=> PERFECT!
     original_orgs = Organization.select(:name).all.map { |row| row.name }.uniq.sort
     @orgs = original_orgs.compact.sort.uniq
     puts @orgs.count
